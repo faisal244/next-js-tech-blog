b6c4daeae15226cf9f831fbf1eb7f6bc6597a7c4
Faisal Mohammed <m.faisal244@gmail.com>
2022-09-20 23:58:07 +0100

    Update [slug].tsx

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,8 +1,3 @@
-import { GetStaticProps } from "next";
-import Header from "../../components/Header";
-import { sanityClient, urlFor } from "../../sanity";
-import { Post } from "../../typings";
-
 interface Props {
 	post: Post;
 }
